c916fd2b22b4cf93bf0b1087e47ffbab020d22bc
sufuwang <1137120948@qq.com>
2025-01-04 20:34:27 +0800

    feat: ssl

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import * as fs from 'fs';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -5,7 +6,12 @@ import { AppFilter } from './app.filter';
 import { AppInterceptor } from './app.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: fs.readFileSync('./ssl/sufu.site.key'),
+      cert: fs.readFileSync('./ssl/sufu.site_bundle.crt'),
+    },
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new AppInterceptor());
   app.useGlobalFilters(new AppFilter());
