1cd662e9708b894574b5205ea8f5d418368a13b1
sufuwang <1137120948@qq.com>
2025-10-29 15:52:50 +0800

    feat: set sameSite to be "none"

--- src/lib/cookies.ts
+++ src/lib/cookies.ts
@@ -3,10 +3,8 @@ import { type CookieOptions as TCookieOptions } from 'express';
 export const CookieOptions: TCookieOptions = {
   httpOnly: true, // 禁止 JS 访问，防 XSS
   secure: process.env.NODE_ENV === 'prod', // 仅 HTTPS 传输
-  sameSite: 'strict', // 防止跨站请求
+  sameSite: 'none', // 允许防止跨站请求
   maxAge: 10 * 60 * 1000, // 10分钟
   domain: process.env.NODE_ENV === 'prod' ? 'admin.sufu.site' : 'localhost', // 可选：允许子域名共享
   path: '/', // 可选：作用路径
 };
-
-console.info('CookieOptions: ', CookieOptions);
