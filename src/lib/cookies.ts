fee1dad0956fcbbda7421c191dee82238be02606
sufuwang <1137120948@qq.com>
2025-10-29 16:56:25 +0800

    feat: set maxAge to be 24 hours

--- src/lib/cookies.ts
+++ src/lib/cookies.ts
@@ -4,7 +4,7 @@ export const CookieOptions: TCookieOptions = {
   httpOnly: true, // 禁止 JS 访问，防 XSS
   secure: process.env.NODE_ENV === 'prod', // 仅 HTTPS 传输
   sameSite: 'none', // 允许防止跨站请求
-  maxAge: 10 * 60 * 1000, // 10分钟
+  maxAge: 24 * 60 * 60 * 1000, // 24h
   domain: process.env.NODE_ENV === 'prod' ? '.sufu.site' : '', // 可选：允许子域名共享
   path: '/', // 可选：作用路径
 };
