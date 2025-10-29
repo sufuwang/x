5eff0e4bda704515515b0bb8f7c70baed6f16f71
sufuwang <1137120948@qq.com>
2025-10-29 16:55:08 +0800

    feat: set domain to be "" in dev

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,14 +8,14 @@ import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 将 ConfigService 设为全局可用，这样就能在任何模块中使用而无需再次导入 ConfigModule [citation:2][citation:5][citation:9]
+      envFilePath: [`.env.${process.env.NODE_ENV}`, '.env.local', '.env'],
+    }),
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '10m' },
-    }),
-    ConfigModule.forRoot({
-      isGlobal: true, // 将 ConfigService 设为全局可用，这样就能在任何模块中使用而无需再次导入 ConfigModule [citation:2][citation:5][citation:9]
-      envFilePath: [`.env.${process.env.NODE_ENV}`, '.env.local', '.env'],
     }),
     RedisModule,
     UserModule,
